2b6e6b2cc178289d0100e27645b89d5c453f1d0d
ser23 <ksa27023@gmail.com>
2025-10-30 23:45:00 +0300

    News page completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import Header from "./Header.js";
 import TabsCollectiion from "./Tabs.js";
+import VideoPlayerCollection from "./VideoPlayer.js";
 
 new Header();
 new TabsCollectiion();
+new VideoPlayerCollection();
